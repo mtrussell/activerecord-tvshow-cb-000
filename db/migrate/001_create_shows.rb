1c4bd847c098a827f031f8f01a2814c648133663
Matt Trussell <smtrussell@gmail.com>
2018-01-28 23:51:35 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,1 +1,12 @@
 class Shows < ActiveRecord::Migration
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      t.string :day
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
